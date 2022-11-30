13d4a5682f4856f61e60bc2a4e7868e966a521d8
deekshavishnoi7 <deekshavishnoi7@gmail.com>
2022-12-01 02:50:07 +0530

    Updating steup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
-
+from setuptools import find_packages
 setup(
     name='DSSS Homework 5',
     version='0.1',
-    packages=[''],
+    packages=find_packages(),
+    install_requires=['numpy', 'Pillow', 'numba', 'turtle', 'random', 'seaborn'],
     url='',
     license='',
     author='Deeksha',
     author_email='deekshavishnoi7@gmail.com',
     description='Setup file for \'Let It Snow\''
-)
+    )
