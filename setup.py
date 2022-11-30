5cac3f72e08b6847ad7da8bd901f835c5c42c432
deekshavishnoi7 <deekshavishnoi7@gmail.com>
2022-12-01 01:45:01 +0530

    dding Setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DSSS Homework 5',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Deeksha',
+    author_email='deekshavishnoi7@gmail.com',
+    description='Setup file for \'Let It Snow\''
+)
