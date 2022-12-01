ca5a4b3aea71c599db8d51ba03b1d8031a47b59c
deekshavishnoi7 <deekshavishnoi7@gmail.com>
2022-12-01 11:51:24 +0530

    updating the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='DSSS Homework 5',
     version='0.1',
-    packages=[''],
+    packages=['turtle','numpy','random'],
     url='',
     license='',
     author='Deeksha',
